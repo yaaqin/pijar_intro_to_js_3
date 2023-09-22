93a52783ad190191cf5d982e9373514e4c1ae2fe
yaaqin <yaqin.me@gmail.com>
2023-09-22 17:18:47 +0700

    [ADD]penambahan exercise 3
    penambahan code pembayaran pln

--- exercise3.js
+++ exercise3.js
@@ -5,3 +5,33 @@
 //hitung nilai
 
 //bayar spp
+
+const jumlahTagihan = 100000
+
+function bayarPLN(totalPayment) {
+    return new Promise((resolve, reject) => {
+      // Simulasi pembayaran PLN
+      setTimeout(() => {
+        if (totalPayment >= jumlahTagihan) {
+            resolve("pembayaran selesai dilakukan")
+        } else {
+            reject("dana yang anda bayarkan kurang!")
+        }
+      }, 3000); // Waktu simulasi pembayaran (2 detik)
+    });
+  }
+  
+  // Menggunakan async/await untuk melakukan pembayaran PLN
+  async function prosesPembayaran(tagihanPLN) {
+  
+    try {
+      console.log('Memulai pembayaran PLN...');
+      const hasilPembayaran = await bayarPLN(tagihanPLN);
+      console.log(hasilPembayaran);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  // Memanggil fungsi untuk melakukan pembayaran
+  prosesPembayaran(100000);
