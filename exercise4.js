de378ad0e49116578ed67e81cdd1b5dba90d6236
yaaqin <yaqin.me@gmail.com>
2023-09-22 20:30:48 +0700

    [ADD]penulisan tugas nomor 4

---
+++ exercise4.js
@@ -0,0 +1,25 @@
+// URL REST API yang ingin diambil datanya
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+// Menggunakan metode fetch untuk mengambil data dari REST API
+fetch(apiUrl)
+  .then((response) => {
+    // Memeriksa status respons HTTP
+    if (!response.ok) {
+      throw new Error('HTTP error ' + response.status);
+    }
+    // Mengubah respons menjadi objek JSON
+    return response.json();
+  })
+  .then((data) => {
+    // Mengambil semua nama (name) dari data yang diterima
+    const names = data.map((user) => user.name);
+
+    // Menampilkan nama-nama ke konsol
+    names.forEach((name, index) => {
+      console.log(`User ${index + 1}: ${name}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Terjadi kesalahan:', error);
+  });
